ec76128a71dbd7592ef3509027b599b92ad0cc0c
jakethecoder95 <95jacob07@gmail.com>
2019-03-04 20:55:49 -0800

    quick sort completed

--- freeCodeCamp/Algorithms/quickSort.js
+++ freeCodeCamp/Algorithms/quickSort.js
@@ -1,79 +1,94 @@
-let sortLeft = true, sortRight = false;
-let array1, cnt = 0;
 
-const quickSort = (start, end, arr) => {
-  const pivot = arr[array.length - 1];
-  let itemFromLeft = arr[start],
-    itemFromRight = arr[end];
+let pivot = -1,
+  pivotIndex = -1;
 
-    
-    if (itemFromLeft >= pivot) {
-      if (itemFromRight > pivot) {
-        quickSort(start, end - 1, arr);
-      } else {
-        swap(start, end, arr);
-        itemFromLeft = arr[start]
-        itemFromRight = arr[end];
-      }
-    }
-
-  if (itemFromRight <= pivot) {
-    if (itemFromLeft < pivot) {
-      quickSort(start+1, end, arr);
-    } else {
-      swap(start, end, arr);
-      itemFromRight = arr[end];
-      itemFromLeft = arr[start];
+// Completes a single partition: int start and int end and the array itself
+const partition = (start, end, arr) => {
+  let foundLeft, foundRight; // index of item
+  for (; start < end; start++) {
+    if (arr[start] > pivot) {
+      foundLeft = start;
+      break;
     }
   }
-
-  if (end-1 > start+1) quickSort(start+1, end-1, arr);
-  else {
-      cnt += 1;
-      console.log(cnt);
-      console.log(arr);
-    // const endToEnd = arr.splice(end, arr.length - 2);
-    // arr.push(...endToEnd);
-    // if (end - 2 === 0) {
-    //   console.log('here')
-    //   if (arr.indexOf(pivot) === 2) {
-    //     swap(start, end-2, arr);
-    //   }
-    // }
-    // if (sortLeft) {
-    //   sortLeft = false
-    //   quickSort(0, end-2, arr);
-    // }
+  for (; end > start; end--) {
+    if (arr[end] < pivot) {
+      foundRight = end;
+      break;
+    }
   }
-  console.log("this comes first");
+  if (foundRight && foundLeft) {
+    swap(foundRight, foundLeft, arr);
+  }
+  if (start < end) {
+    return partition(start, end, arr);
+  }
+  swap(pivotIndex, end, arr);
+  return end;
 };
 
-const array = [1, 4, 2, 8];
+// Recursive call to partition to sort left first and then right.
+//  int start, int end, array
+function quickSort(start, end, arr) {
+  if (start < end) {
+    let j = partition(start, end - 1, setPivot(start, end, arr));
+    console.log(arr);
+    quickSort(start, j-2, arr);
+    quickSort(j + 1, end, arr);
+  }
+}
 
-quickSort(0, array.length - 2, getPivot(0, array.length - 1, array));
+const array = [
+  1,
+  4,
+  2,
+  8,
+  345,
+  123,
+  43,
+  32,
+  5643,
+  63,
+  123,
+  43,
+  2,
+  55,
+  1,
+  234,
+  92
+];
 
-function getPivot(start, end, arr) {
-  let pivot;
+quickSort(0, array.length - 1, array);
+console.log(array);
+
+// HELPER FUNCTIONS
+
+// setPivot finds Pivot and moves it out of the way.
+function setPivot(start, end, arr) {
   const first = arr[start];
   const last = arr[end];
   const mid = arr[Math.floor(end / 2)];
   if ((first <= last && first >= mid) || (first <= mid && first >= last)) {
-    pivot = start;
+    pivotIndex = start;
   }
   if ((mid <= first && mid >= last) || (mid <= last && mid >= first)) {
-    pivot = Math.floor(end / 2);
+    pivotIndex = Math.floor(end / 2);
   }
   if ((last <= first && last >= mid) || (last <= mid && last >= first)) {
-    pivot = end;
+    pivotIndex = end;
   }
 
-  if (end !== pivot) {
-    swap(pivot, end, arr);
+  pivot = arr[pivotIndex];
+  if (end !== pivotIndex) {
+    swap(pivotIndex, end, arr);
+    pivotIndex = end;
   }
 
   return arr;
 }
 
+// Swaps values: i1 = index value 1,
+//               i2 = index value 2
 function swap(i1, i2, arr) {
   const holdin = array[i2];
   arr[i2] = arr[i1];
