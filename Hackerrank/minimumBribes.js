bab44152cb67436c702dfd591e9d7f5f5eacfba1
Jacob Robinson <95jacob07@gmail.com>
2019-03-16 23:51:14 -0700

    minimumBribes initiated

---
+++ Hackerrank/minimumBribes.js
@@ -0,0 +1,13 @@
+/** 
+** Any person in the queue can bribe the person directly in front of 
+** them to swap positions. If two people swap positions, they still wear
+** the same sticker denoting their original places in line. One person
+** can bribe at most two others. For example, if and  bribes , the queue
+** will look like this: .
+
+** Fascinated by this chaotic queue, you decide you must know the minimum
+** number of bribes that took place to get the queue into its current state!
+**/
+
+// Complete the minimumBribes function below.
+function minimumBribes(q) {}
