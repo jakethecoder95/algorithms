cbca3cdce4831848f037a6d5fd702a1f8af03d3c
jakethecoder95 <95jacob07@gmail.com>
2019-03-17 22:56:35 -0700

    minimumBribes.js challenge complete

--- Hackerrank/arrayRotate.js
+++ Hackerrank/arrayRotate.js
@@ -1,9 +1,9 @@
 // Complete the rotLeft function below.
 function rotLeft(a, d) {
-  const back = a.splice(0, 3);
-  a.push(...back);
+  a.push(...a.splice(0, d));
+  return a;
 }
 
 let a = [1, 2, 3, 4, 5, 6];
-rotLeft(a, 3);
-console.log(a);
+console.log(rotLeft(a, 3));
+// console.log(a);
--- Hackerrank/minimumBribes.js
+++ Hackerrank/minimumBribes.js
@@ -10,4 +10,43 @@
 **/
 
 // Complete the minimumBribes function below.
-function minimumBribes(q) {}
+function minimumBribes(q) {
+  let length = q.length - 1,
+    result = 0;
+
+  for (let i = length; i > 0; ) {
+    if (q[i] < q[i - 1]) {
+      swap(i, i - 1, q);
+      result++;
+      if (q[i] > q[i + 1]) {
+        swap(i, i + 1, q);
+        result++;
+        if (q[i + 1] > q[i + 2]) {
+          result = "Too chaotic";
+          break;
+        }
+      }
+    } else {
+      i--;
+    }
+  }
+
+  console.log(result);
+}
+
+function swap(i1, i2, q) {
+  const hold = q[i1];
+  q[i1] = q[i2];
+  q[i2] = hold;
+}
+
+minimumBribes([2, 5, 1, 3, 4]);
+
+// swaps: 4
+// 1 2 5 3 4 7 6 8
+
+// 1 2 5 3 4 6 7 8
+
+// 1 2 3 5 4 6 7 8
+
+// 1 2 3 4 5 6 7 8
