e36241a7d9e9a3e100890ea22c6a9645d2f91387
Jacob Robinson <95jacob07@gmail.com>
2019-10-31 10:39:28 -0700

    TwoSum in C#

--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -6,14 +6,7 @@ namespace dotnet
     {
         static void Main(string[] args)
         {
-            var numbers = new int[] { 1, 3, 4, 5, 6, 6, 7, 9 };
 
-            var matches = TwoSum.Run(numbers, 10);
-
-            foreach (var match in matches)
-            {
-                Console.Write(match + ", ");
-            }
         }
     }
 }
